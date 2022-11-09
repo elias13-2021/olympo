a520fb76a4724a2f1ba2f972c5ff142f1682ba07
elias13-2021 <89867280+elias13-2021@users.noreply.github.com>
2022-11-09 15:51:34 -0300

    creando archivo 2

---
+++ webapp/urls.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.urls import path
+
+from django import views
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path("login/", login_request, name="login"),
+	path("logout/", logout_request, name= "logout"),
+    path('verClientes', verClientes, name='verClientes'),
+    path('agregarCliente', agregarCliente),
+    path('editarCliente/<int:DNI>', editarCliente),
+    path('detalleCliente/<int:DNI>', detalleCliente),
+    path('agregarInstructor', agregarInstructor),
+    path('verInstructores', verInstructores, name="instructores"),
+    path('editarInstructor/<int:DNIinstructor>', editarInstructor),
+    path('agregarModalidad', agregarModalidad),
+    path('verModalidades', verModalidades, name="verModalidades"),
+    path('editarModalidad/<int:codmodalidad>', editarModalidad),
+    path('agregarDia', agregarDia),
+    path('verDias', verDias, name="verDias"),
+    path('verActividades', verActividad, name='actividad'),
+    path('agregarActividad', agregarActividad),
+    path('detalleActividad/<int:codactividad>', detalleActividad),
+    path('editarActividad/<int:codactividad>', editarActividad),
+    path('verRegistros', verRegistros, name='registros'),
+    path('agregarRegistro', agregarRegistro),
+    path('editarRegistro/<int:codregistro>', editarRegistro),
+    path('detalleRegistro/<int:codregistro>', detalleRegistro),
+    path('listadoRegistroTM1', listadoRegistroTM1),
+    path('listadoRegistroTM2', listadoRegistroTM2),
+]
