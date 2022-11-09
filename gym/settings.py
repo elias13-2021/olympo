3e930d165aa5f52282fcf6b8a53d578b8a589007
elias13-2021 <89867280+elias13-2021@users.noreply.github.com>
2022-11-09 18:12:40 -0300

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pyegcj!v-4z#17v_hrat$j%n-4dof+09v1cwjilx+w^vu)2cxn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'gymolympo.pythonanywhere.com']
 
 
 # Application definition
